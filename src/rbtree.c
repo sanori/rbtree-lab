88e831ecc0cb91de29485560f75633859c7f9ed0
Joo-Won Jung <sanori@gmail.com>
2021-08-26 17:04:04 +0900

    feat: first draft

---
+++ src/rbtree.c
@@ -0,0 +1,33 @@
+#include "rbtree.h"
+
+rbtree* new_rbtree() {
+    
+}
+
+int delete_rbtree(rbtree* t) {
+    return 0;
+}
+
+int rbtree_insert(rbtree* t, key_t key) {
+    return 0;
+}
+
+node_t *rbtree_find(rbtree* t, key_t key) {
+
+}
+
+node_t *rbtree_min(rbtree* t) {
+
+}
+
+node_t *rbtree_max(rbtree* t) {
+
+}
+
+int rbtree_erase(rbtree* t, node_t* p) {
+
+}
+
+int rbtree_to_array(rbtree* t, key_t* arr, int n) {
+
+}
---
+++ src/rbtree.h
@@ -0,0 +1,23 @@
+
+typedef enum {RED, BLACK} color_t;
+typedef int key_t; 
+typedef struct node_t {
+    color_t color;
+    key_t key;
+    struct node_t *parent, *left, *right;
+} node_t;
+
+typedef struct {
+  node_t *root;
+} rbtree;
+
+rbtree* new_rbtree();
+int delete_rbtree(rbtree*);
+
+int rbtree_insert(rbtree*, key_t);
+node_t *rbtree_find(rbtree*, key_t);
+node_t *rbtree_min(rbtree*);
+node_t *rbtree_max(rbtree*);
+int rbtree_erase(rbtree*, node_t*);
+
+int rbtree_to_array(rbtree*, key_t*, int);
