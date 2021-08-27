67176b9aa5aa10cd49ac45e1dfcbc2466fec4230
Joo-Won Jung <sanori@gmail.com>
2021-08-28 01:04:17 +0900

    refactor: enhance type definitions and apply google style guide

--- src/rbtree.c
+++ src/rbtree.c
@@ -1,36 +1,43 @@
-#include <malloc.h>
 #include "rbtree.h"
 
-rbtree* new_rbtree() {
-    rbtree *p = (rbtree *) calloc(sizeof(rbtree), 1);
-    return p;
+#include <malloc.h>
+
+rbtree *new_rbtree(void) {
+  rbtree *p = (rbtree *)calloc(sizeof(rbtree), 1);
+  return p;
 }
 
-void delete_rbtree(rbtree* t) {
-    // TODO: reclaim the tree nodes's memory
-    free(t);
+void delete_rbtree(rbtree *t) {
+  // TODO: reclaim the tree nodes's memory
+  free(t);
 }
 
-node_t *rbtree_insert(rbtree* t, key_t key) {
-    return t->root;
+node_t *rbtree_insert(const rbtree *t, const key_t key) {
+  // TODO: implement insert
+  return t->root;
 }
 
-node_t *rbtree_find(rbtree* t, key_t key) {
-    return t->root;
+node_t *rbtree_find(const rbtree *t, const key_t key) {
+  // TODO: implement find
+  return t->root;
 }
 
-node_t *rbtree_min(rbtree* t) {
-    return t->root;
+node_t *rbtree_min(const rbtree *t) {
+  // TODO: implement find
+  return t->root;
 }
 
-node_t *rbtree_max(rbtree* t) {
-    return t->root;
+node_t *rbtree_max(const rbtree *t) {
+  // TODO: implement find
+  return t->root;
 }
 
-int rbtree_erase(rbtree* t, node_t* p) {
-    return 0;
+int rbtree_erase(const rbtree *t, node_t *p) {
+  // TODO: implement erase
+  return 0;
 }
 
-int rbtree_to_array(rbtree* t, key_t* arr, int n) {
-    return 0;
+int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
+  // TODO: implement to_array
+  return 0;
 }
--- src/rbtree.h
+++ src/rbtree.h
@@ -1,24 +1,31 @@
+#ifndef RBTREE_H_
+#define RBTREE_H_
+
 #include <stddef.h>
 
-typedef enum {RED, BLACK} color_t;
-typedef int key_t; 
+typedef enum { RBTREE_RED, RBTREE_BLACK } color_t;
+
+typedef int key_t;
+
 typedef struct node_t {
-    color_t color;
-    key_t key;
-    struct node_t *parent, *left, *right;
+  color_t color;
+  key_t key;
+  struct node_t *parent, *left, *right;
 } node_t;
 
 typedef struct {
   node_t *root;
 } rbtree;
 
-rbtree* new_rbtree();
-void delete_rbtree(rbtree*);
+rbtree *new_rbtree(void);
+void delete_rbtree(rbtree *);
 
-node_t *rbtree_insert(rbtree*, key_t);
-node_t *rbtree_find(rbtree*, key_t);
-node_t *rbtree_min(rbtree*);
-node_t *rbtree_max(rbtree*);
-int rbtree_erase(rbtree*, node_t*);
+node_t *rbtree_insert(const rbtree *, const key_t);
+node_t *rbtree_find(const rbtree *, const key_t);
+node_t *rbtree_min(const rbtree *);
+node_t *rbtree_max(const rbtree *);
+int rbtree_erase(const rbtree *, node_t *);
 
-int rbtree_to_array(rbtree*, key_t*, int);
+int rbtree_to_array(const rbtree *, key_t *, const size_t);
+
+#endif  // RBTREE_H_
