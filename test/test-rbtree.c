b99cacc393de33c0dab549171ff17bbecab62d9a
Joo-Won Jung <sanori@gmail.com>
2021-09-02 12:36:03 +0900

    feat: add test cases for min, max, to_array

--- test/test-rbtree.c
+++ test/test-rbtree.c
@@ -2,6 +2,7 @@
 #include <rbtree.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 // new_rbtree should return rbtree struct with null root node
 void test_init(void) {
@@ -51,6 +52,74 @@ void test_erase_root(const key_t key) {
 
   rbtree_erase(t, p);
   assert(t->root == NULL);
+
+  delete_rbtree(t);
+}
+
+static void insert_arr(const rbtree *t, const key_t *arr, const size_t n) {
+  for (size_t i = 0; i < n; i++) {
+    rbtree_insert(t, arr[i]);
+  }
+}
+
+static int comp(const void *p1, const void *p2) {
+  const key_t *e1 = (const key_t *) p1;
+  const key_t *e2 = (const key_t *) p2;
+  if (*e1 < *e2) {
+    return -1;
+  } else if (*e1 > *e2) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+// min/max should return the min/max value of the tree
+void test_minmax(key_t *arr, const size_t n) {
+  rbtree *t = new_rbtree();
+  assert(t != NULL);
+
+  insert_arr(t, arr, n);
+  assert(t->root != NULL);
+
+  qsort((void *) arr, n, sizeof(key_t), comp);
+  node_t *p = rbtree_min(t);
+  assert(p != NULL);
+  assert(p->key == arr[0]);
+
+  node_t *q = rbtree_max(t);
+  assert(q != NULL);
+  assert(q->key == arr[n - 1]);
+
+  if (n >= 1) {
+    rbtree_erase(t, p);
+    p = rbtree_min(t);
+    assert(p != NULL);
+    assert(p->key == arr[1]);
+  }
+
+  if (n >= 2) {
+    rbtree_erase(t, q);
+    q = rbtree_max(t);
+    assert(q != NULL);
+    assert(q->key == arr[n - 2]);
+  }
+
+  delete_rbtree(t);
+}
+
+void test_to_array(const key_t *arr, const size_t n) {
+  rbtree *t = new_rbtree();
+  assert(t != NULL);
+
+  insert_arr(t, arr, n);
+  qsort((void *)arr, n, sizeof(key_t), comp);
+
+  key_t *res = calloc(n, sizeof(key_t));
+  rbtree_to_array(t, res, n);
+  for (int i = 0; i < n; i++) {
+    assert(arr[i] == res[i]);
+  }
 
   delete_rbtree(t);
 }
@@ -139,9 +208,7 @@ void test_rb_constraints(const key_t arr[], const size_t n) {
   rbtree *t = new_rbtree();
   assert(t != NULL);
 
-  for (int i = 0; i < n; i++) {
-    rbtree_insert(t, arr[i]);
-  }
+  insert_arr(t, arr, n);
   assert(t->root != NULL);
 
   test_color_constraint(t);
@@ -162,12 +229,19 @@ void test_duplicate_values() {
   test_rb_constraints(entries, n);
 }
 
+void test_minmax_suite() {
+  key_t entries[] = {10, 5, 8, 34, 67, 23, 156, 24, 2, 12};
+  const size_t n = sizeof(entries) / sizeof(entries[0]);
+  test_minmax(entries, n);
+}
+
 int main(void) {
   test_init();
   test_insert_single(1024);
   test_find_single(512, 1024);
   test_erase_root(128);
+  test_minmax_suite();
   test_distinct_values();
   test_duplicate_values();
-  printf("Passed all tests\n");
+  printf("Passed all tests!\n");
 }
