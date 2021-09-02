84cbaa89af8eed6fd01ef797273b3222df90a9b7
Joo-Won Jung <sanori@gmail.com>
2021-09-02 11:21:29 +0900

    feat: add test cases for find, erase and duplicate values

--- test/test-rbtree.c
+++ test/test-rbtree.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <rbtree.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 // new_rbtree should return rbtree struct with null root node
 void test_init(void) {
@@ -10,7 +11,7 @@ void test_init(void) {
   delete_rbtree(t);
 }
 
-// root node constraint
+// root node should have proper values and pointers
 void test_insert_single(const key_t key) {
   rbtree *t = new_rbtree();
   node_t *p = rbtree_insert(t, key);
@@ -24,9 +25,40 @@ void test_insert_single(const key_t key) {
   delete_rbtree(t);
 }
 
+// find should return the node with the key or NULL if no such node exists
+void test_find_single(const key_t key, const key_t wrong_key) {
+  rbtree *t = new_rbtree();
+  node_t *p = rbtree_insert(t, key);
+
+  node_t *q = rbtree_find(t, key);
+  assert(q != NULL);
+  assert(q->key == key);
+  assert(q == p);
+
+  q = rbtree_find(t, wrong_key);
+  assert(q == NULL);
+
+  delete_rbtree(t);
+}
+
+// erase should delete root node
+void test_erase_root(const key_t key) {
+  rbtree *t = new_rbtree();
+  node_t *p = rbtree_insert(t, key);
+  assert(p != NULL);
+  assert(t->root != p);
+  assert(p->key == key);
+
+  rbtree_erase(t, p);
+  assert(t->root == NULL);
+
+  delete_rbtree(t);
+}
+
 // Search tree constraint
 // The values of left subtree should be less than or equal to the current node
-// The values of right subtree should be greater than or equal to the current node
+// The values of right subtree should be greater than or equal to the current
+// node
 
 static bool search_traverse(const node_t *p, key_t *min, key_t *max) {
   if (p == NULL) {
@@ -118,10 +150,24 @@ void test_rb_constraints(const key_t arr[], const size_t n) {
   delete_rbtree(t);
 }
 
-int main(void) {
-  test_init();
-  test_insert_single(1024);
+void test_distinct_values() {
   const key_t entries[] = {10, 5, 8, 34, 67, 23, 156, 24, 2, 12};
   const size_t n = sizeof(entries) / sizeof(entries[0]);
   test_rb_constraints(entries, n);
+}
+
+void test_duplicate_values() {
+  const key_t entries[] = {10, 5, 5, 34, 6, 23, 12, 12, 6, 12};
+  const size_t n = sizeof(entries) / sizeof(entries[0]);
+  test_rb_constraints(entries, n);
+}
+
+int main(void) {
+  test_init();
+  test_insert_single(1024);
+  test_find_single(512, 1024);
+  test_erase_root(128);
+  test_distinct_values();
+  test_duplicate_values();
+  printf("Passed all tests\n");
 }
